30d6e36e7e7dd077590eb83ce88eba61a895711e
ogbaududavid <ogbaudu.dc@gmail.com>
2022-10-31 22:50:54 +0100

    added visit count

--- lesson7/chamber/scripts/discover.js
+++ lesson7/chamber/scripts/discover.js
@@ -1,26 +1,21 @@
-const last_visit = new Date();
-let current_visit = new Date();
+let current_visit = window.localStorage.getItem("current_visit");
+let last_visit = window.localStorage.getItem("last_visit");
+let p = document.getElementsByClassName("show-visits")[0];
+let time = new Date().getTime()
+let day = parseInt(time/(1000 * 3600 * 24));
+let diff_in_days = current_visit - last_visit;
 
-let numOfDays = current_visit - last_visit;
-numVisits++
-
-
-// get the stored value in localStorage
-let days = Number(window.localStorage.getItem("days"));
-
-// determine if this is the first visit or display the number of visits.
-if (days!== 0) {
-	alert("The last time you visited this page was" + days + "day(s) ago");
-} else {
-	alert("This is your first visit!");
+if (diff_in_days == 0){
+	p.append("It has been 0 day(s) since your last visit")
+	window.localStorage.setItem("last_visit", current_visit);
 }
-localStorage.setItem("days", numOfDays);
-
-
-if ((last_visit - current_visit) != 0 ) {
-localStorage.setItem("last_visit", numOfDays);
+else if(diff_in_days === NaN){
+	p.append("LOCO")
+}
+else {
+	p.append("It has been " + diff_in_days + " day(s) since your last visit");
+	window.localStorage.setItem("last_visit", current_visit);
 }
 
-
-
-
+window.localStorage.setItem("current_visit", day);
+window.localStorage.setItem("diff_in_days", diff_in_days) 
