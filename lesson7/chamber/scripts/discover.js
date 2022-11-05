7ce5cf1e0f0b1db400bba67615f30eb744b6e23c
ogbaududavid <ogbaudu.dc@gmail.com>
2022-11-05 10:17:46 +0100

    .completed instructed canges

--- lesson7/chamber/scripts/discover.js
+++ lesson7/chamber/scripts/discover.js
@@ -1,37 +1,37 @@
-let current_visit = window.localStorage.getItem("current_visit");
-let last_visit = window.localStorage.getItem("last_visit");
-let p = document.getElementsByClassName("show-visits")[0];
+let currentVisit = window.localStorage.getItem("currentVisit");
+let lastVisit = window.localStorage.getItem("lastVisit");
+let p = document.getElementsByClassName("showVisits")[0];
 let time = new Date().getTime()
 let day = parseInt(time/(1000 * 3600 * 24));
-let diff_in_days = current_visit - last_visit;
+let diffInDays = currentVisit - lastVisit;
+getLastVisit()
 
-if (diff_in_days == 0){
-	p.append("It has been 0 day(s) since your last visit")
-	window.localStorage.setItem("last_visit", current_visit);
-}
-else if (isNaN(diff_in_days)){
-	p.append("it has been 0 day(s) since your last visit")
-	window.localStorage.setItem("last_visit", current_visit);
-}
-else {
-	p.append("It has been " + diff_in_days + " day(s) since your last visit");
-	window.localStorage.setItem("last_visit", current_visit);
-}
+function getLastVisit(){
+  if (diffInDays == 0){
+    p.append("It has been 0 day(s) since your last visit")
+    window.localStorage.setItem("lastVisit", currentVisit);
+  }
+  else if (isNaN(diffInDays)){
+    p.append("it has been 0 day(s) since your last visit")
+    window.localStorage.setItem("lastVisit", currentVisit);
+  }
+  else {
+    p.append("It has been " + diffInDays + " day(s) since your last visit");
+    window.localStorage.setItem("lastVisit", currentVisit);
+  }
 
-window.localStorage.setItem("current_visit", day);
-window.localStorage.setItem("diff_in_days", diff_in_days) 
-
+  window.localStorage.setItem("currentVisit", day);
+  window.localStorage.setItem("diffInDays", diffInDays) 
+}
 
 
 let imagesToLoad = document.querySelectorAll("source[data-srcset]");
-
 const loadImages = (image) => {
   image.setAttribute("srcset", image.getAttribute("data-srcset"));
   image.onload = () =>{
     image.removeAttribute("data-srcset");
   };
 };
-
 if ("IntersectionObserver" in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
