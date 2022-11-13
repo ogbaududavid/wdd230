02103ba46d90c9d363bc84dc6ec3f57b480cf7a2
ogbaududavid <ogbaudu.dc@gmail.com>
2022-11-13 17:32:46 +0100

    Implemented instrcuted changes in join page

--- lesson9/chamber/scripts/index.js
+++ lesson9/chamber/scripts/index.js
@@ -1,18 +1,21 @@
 function getDate(){
-let now = new Date();
-let today = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-document.getElementsByClassName("show-date")[0].innerHTML= today;
+	let now = new Date();
+	let today = new Intl.DateTimeFormat("en-UK", {
+		dateStyle: "full"
+	}).format(now);
+	document.getElementsByClassName("show-date")[0].innerHTML= today;
 }
+
 function toggleMenu(){
-document.getElementsByClassName("links")[0].classList.toggle("responsive");
+	document.getElementsByClassName("links")[0].classList.toggle("responsive");
 }
+
 function getFooter(){
-let mod= document.lastModified;
-text = "lekkicoc.com &copy; 2022 .:|:. David Ogbaudu .:|:. WDD230 .:|:. Last Updated:" + mod;
-document.getElementsByClassName("footer-text")[0].innerHTML = text;
+	let mod= document.lastModified;
+	text = "lekkicoc.com &copy; 2022 .:|:. David Ogbaudu .:|:. WDD230 .:|:. Last Updated:" + mod;
+	document.getElementsByClassName("footer-text")[0].innerHTML = text;
 }
+
 function getEvent(){
 	let now = new Date();
 	const day = now.getDay()
@@ -22,4 +25,11 @@ function getEvent(){
 		banner.style.color= "black";
 		banner.style.backgroundColor="pink";	
 }
+}
+
+function showDate(){
+	let showDate = document.getElementsByClassName("load-time")[0];
+	let input = document.createElement("input");
+	input.value = new Date();
+	showDate.appendChild(input);
 }
