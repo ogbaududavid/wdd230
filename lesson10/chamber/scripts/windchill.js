4822fc3779d48c10ab78fc0996a38be8e895e619
ogbaududavid <ogbaudu.dc@gmail.com>
2022-11-18 17:41:53 +0100

    .

--- lesson10/chamber/scripts/windchill.js
+++ lesson10/chamber/scripts/windchill.js
@@ -1,4 +1,5 @@
-const weatherUrl =  "https://api.openweathermap.org/data/2.5/weather?id=2332453&appid=b6f2d588b3f237931066c85d53d0e44c&units=imperial"
+//const weatherUrl =  "https://api.openweathermap.org/data/2.5/weather?id=2332453&appid=b6f2d588b3f237931066c85d53d0e44c&units=imperial"
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=41.139981&lon=-104.820246&units=imperial&appid=b6f2d588b3f237931066c85d53d0e44c"
 async function apiFetch(){
   try{
     const response = await fetch(weatherUrl);
@@ -18,24 +19,18 @@ async function apiFetch(){
 apiFetch()
 
 function displayResults(weatherData) {
-  const currentTemperature = document.getElementById("temperature");
-  const windSpeed = document.getElementById("wind-speed");
-  let windChill = document.getElementById("result");
+  var currentTemperature = document.getElementById("temperature");
+  var windSpeed = document.getElementById("wind-speed");
+  const windChill = document.getElementById("result");
   const currentCondition = document.getElementById("condition");
   const weatherIcon = document.getElementById("weather-icon");
   const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
   const desc = weatherData.weather[0].description;
 
-  currentTemperature.innerHTML = weatherData.main.temp.toFixed(0);
+  currentTemperature.innerHTML = parseFloat(weatherData.main.temp.toFixed(0));
   weatherIcon.setAttribute("src", iconsrc)
   weatherIcon.setAttribute("alt", desc)
-  windSpeed.innerHTML = weatherData.wind.speed.toFixed(0);
+  windSpeed.innerHTML = parseFloat(weatherData.wind.speed.toFixed(0));
   currentCondition.innerHTML = weatherData.weather[0].description;
-  let chill =  35.74 + (0.6215 * currentTemperature) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * currentTemperature * (windSpeed ** 0.16));
-  if (isNaN(chill)) {
-    windChill.innerHTML="N/A";
-  }
-  else{
-    windChill.innerHTML = chill;
-  }
+  windChill.innerHTML =  35.74 + 0.6215 * currentTemperature - 35.75 * windSpeed ** 0.16 + 0.4275 * currentTemperature * windSpeed ** 0.16;
 }
