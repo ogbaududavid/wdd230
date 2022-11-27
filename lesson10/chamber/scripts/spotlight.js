c84039ddeb4c5417fa98c9b08dd0fd2fa9250183
ogbaududavid <ogbaudu.dc@gmail.com>
2022-11-27 14:49:36 +0100

    Made changes to styling

--- lesson10/chamber/scripts/spotlight.js
+++ lesson10/chamber/scripts/spotlight.js
@@ -59,7 +59,7 @@ function getGoldSilverMembers(business){
 }
 
 function createRandomList(){
-    while (i<4){
+    while (i<5){
         let itemValue = orderList[Math.floor(Math.random()*orderList.length)];
         if ( randomList.includes(itemValue) ){
             i = i;
