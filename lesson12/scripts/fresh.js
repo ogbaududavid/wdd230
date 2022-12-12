0d467fc1a9d9e4dcf608fbc60bd591b4c38d7641
ogbaududavid <ogbaudu.dc@gmail.com>
2022-12-12 20:04:31 +0100

    Added font style to all pages

--- lesson12/scripts/fresh.js
+++ lesson12/scripts/fresh.js
@@ -2,6 +2,7 @@ const requestURL = "scripts/fruit.json";
 const select1 = document.querySelector("#sfruit1");
 const select2 = document.querySelector("#sfruit2");
 const select3 = document.querySelector("#sfruit3");
+const submitBtn = document.querySelector("#submitBtn")
 
 
 fetch(requestURL)
@@ -21,16 +22,23 @@ function displayFruit(fruit){
 
 
     let fruitName = fruit.name;
-    option1.setAttribute("value", fruitName)
+    option1.setAttribute("value", fruitName.toLowerCase());
+    option2.setAttribute("value", fruitName);
+    option3.setAttribute("value", fruitName);
 
-    option2.setAttribute("value", fruitName)
-    option3.setAttribute("value", fruitName)
 
-    option1.textContent = fruitName
-    option2.textContent = fruitName
-    option3.textContent = fruitName
-    
-    select1.appendChild(option1)
-    select2.appendChild(option2)
-    select3.appendChild(option3)
+    option1.textContent = fruitName;
+    option2.textContent = fruitName;
+    option3.textContent = fruitName;
+
+    select1.appendChild(option1);
+    select2.appendChild(option2);
+    select3.appendChild(option3);
+
+    //select1.addEventListener("click", function () {console.log(select1.options[select1.selectedIndex].text);})
+}
+
+document.getElementById("#sfruit1").addEventListener("click", showInfo)
+function showInfo(){
+    alert(document.getElementById("#sfruit1").options[sfruit1.selectedIndex].text)
 }
