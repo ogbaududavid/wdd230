70f7dc0f0ef651bb29346f4779ef0f76921e227a
ogbaududavid <ogbaudu.dc@gmail.com>
2022-12-14 17:11:09 +0100

    More styling

--- lesson12/scripts/weather.js
+++ lesson12/scripts/weather.js
@@ -1,10 +1,27 @@
-const weatherUrl =  "https://api.openweathermap.org/data/2.5/weather?id=5334223&appid=b6f2d588b3f237931066c85d53d0e44c&units=imperial"
+//const weatherUrl =  "https://api.openweathermap.org/data/2.5/weather?id=5334223&appid=b6f2d588b3f237931066c85d53d0e44c&units=imperial"
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=33.1581&lon=-117.3506&appid=b6f2d588b3f237931066c85d53d0e44c"
+const weatherUrl2 = "https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&appid=b6f2d588b3f237931066c85d53d0e44c"
 async function apiFetch(){
   try{
     const response = await fetch(weatherUrl);
     if (response.ok){
       const data = await response.json();
       displayResults(data);
+    }
+    else {
+      throw Error (await response.text());
+    }
+  }
+  catch (error){
+    console.log(error);
+  }
+
+  try{
+    const response = await fetch(weatherUrl2);
+    if (response.ok){
+      const data = await response.json();
+      tempForecast(data);
+      console.log(data)
     }
     else {
       throw Error (await response.text());
@@ -39,4 +56,24 @@ function displayResults(weatherData) {
   else{
     document.getElementById("result").innerHTML = windChill;
   }
+}
+
+function tempForecast(day){
+  const day1 = day.list[5].main.temp.toFixed(0);
+  const day2 = day.list[11].main.temp.toFixed(0);
+  const day3 = day.list[20].main.temp.toFixed(0);
+  const weather2 = document.getElementsByClassName("weather2")[0];
+
+  let p1 = document.createElement("p");
+  let p2 = document.createElement("p");
+  let p3 = document.createElement("p");
+  p1.textContent = "day1} " + <span>&#176;F</span>;
+  p2.textContent = day2;
+  p3.textContent = day3;
+  weather2.appendChild(p1);
+  weather2.appendChild(p2);
+  weather2.appendChild(p3);
+
+
+
 }
