e15f1e6d989128da469c3719a908d992d3ea3417
ogbaududavid <ogbaudu.dc@gmail.com>
2022-12-14 17:43:51 +0100

    .

--- lesson12/scripts/weather.js
+++ lesson12/scripts/weather.js
@@ -21,7 +21,6 @@ async function apiFetch(){
     if (response.ok){
       const data = await response.json();
       tempForecast(data);
-      console.log(data)
     }
     else {
       throw Error (await response.text());
